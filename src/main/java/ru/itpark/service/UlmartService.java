24c9e24b1987a0a35c71a18f4fc6191b0fe78c2e
Damir <alekbaevd@yandex.ru>
2019-01-14 18:54:12 +0300

    Fixed #4, #5, #6, #7

--- src/main/java/ru/itpark/service/UlmartService.java
+++ src/main/java/ru/itpark/service/UlmartService.java
@@ -3,10 +3,10 @@ package ru.itpark.service;
 import ru.itpark.domain.Product;
 import ru.itpark.repository.UlmartRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UlmartService {
     private UlmartRepository repository;
@@ -26,38 +26,29 @@ public class UlmartService {
     public List<Product> getSorted(Comparator<Product> comparator) {
         List<Product> result = repository.getAll();
         result.sort(comparator);
+
         return result;
     }
 
     public List<Product> findByFirm(String firm) {
-        List<Product> result = new ArrayList<>();
-        List<String> findByFirmList = Arrays.asList(firm);
-        for (Product product : repository.getAll()) {
-            if (findByFirmList.contains(product.getFirm())) {
-                result.add(product);
-            }
-        }
-        return result;
+        List<String> findByFirmList = Collections.singletonList(firm);
+
+        return repository.getAll().stream()
+                .filter(product -> findByFirmList.contains(product.getFirm()))
+                .collect(Collectors.toList());
     }
 
     public List<Product> findByCategory(String category) {
-        List<Product> result = new ArrayList<>();
-        List<String> findByCategoryList = Arrays.asList(category);
-        for (Product product : repository.getAll()) {
-            if (findByCategoryList.contains(product.getCategory())) {
-                result.add(product);
-            }
-        }
-        return result;
+        List<String> findByCategoryList = Collections.singletonList(category);
+
+        return repository.getAll().stream()
+                .filter(product -> findByCategoryList.contains(product.getCategory()))
+                .collect(Collectors.toList());
     }
 
     public List<Product> findByPriceRange(int priceMin, int priceMax) {
-        List<Product> result = new ArrayList<>();
-        for (Product product : repository.getAll()) {
-            if (product.getPrice() >= priceMin && product.getPrice() <= priceMax) {
-                result.add(product);
-            }
-        }
-        return result;
+        return repository.getAll().stream()
+                .filter(product -> product.getPrice() >= priceMin && product.getPrice() <= priceMax)
+                .collect(Collectors.toList());
     }
 }
